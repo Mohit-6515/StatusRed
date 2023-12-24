980e538001d13ff7d2bdddb0449fd0738ca9bfb2
Mohit-6515 <f20201707@goa.bits-pilani.ac.in>
2023-12-25 01:58:58 +0530

    adding js files

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_iqmch1ZEc', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '77df1altiir4j3osrbrjinqn9n', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://nu1iau6n8j.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
